a337c8f78b6d2a3459bd9810e4a5e632c284a7c2
Ramirojj <126816654+Ramirojj@users.noreply.github.com>
2025-02-10 21:03:39 -0600

    [2.08] Uses data attribute to access product name in toast

--- discount.js
+++ discount.js
@@ -1,7 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById('discount-row').addEventListener('click', function(e){
-        if (e.target.classList.contains('discount')) {
-            e.preventDefault();
-            bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast')).show();
-});
-});
+document.getElementById('discount-row').addEventListener('click', function(e){
+    if (e.target.classList.contains('discount')) {
+      e.preventDefault();
+      document.getElementById('product').innerHTML = e.target.dataset['product'];
+      bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast')).show();
+    }
+  });
